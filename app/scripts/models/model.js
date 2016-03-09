6ae8eb6a9f08ffb89800f747c4e3a3c80d287873
Bruemor <arymer1@gmail.com>
2016-03-09 17:58:38 -0500

    Model, view, index updates

--- app/scripts/models/model.js
+++ app/scripts/models/model.js
@@ -6,8 +6,11 @@ var Contact = Backbone.Model.extend({
 });
 
 var ContactInfo = Backbone.Collection.extend({
-  model: Contact
+  model: Contact,
+  url: 'http://tiny-lasagna-server.herokuapp.com/collections/contact-app'
 });
+
+var ContactData = new Contact();
 
 module.exports = {
   'Contact': Contact,
